ca1f4e88495f1d2ca441367e5ed461581d2d17a4
mmatte200 <mmatte200@users.noreply.github.com>
2025-11-29 11:11:17 -0500

    Initial commit

---
+++ src/main/java/org/m_mattei/App.java
@@ -0,0 +1,13 @@
+package org.m_mattei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
